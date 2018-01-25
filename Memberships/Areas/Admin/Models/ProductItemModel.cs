45ca9ea2c83cb8c3e6c02689081f5b160299f500
amanda li <amanda.li@xero.com>
2018-01-25 11:23:42 +1100

    initial commit

---
+++ Memberships/Areas/Admin/Models/ProductItemModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using Memberships.Entities;
+namespace Memberships.Areas.Admin.Models
+{
+    public class ProductItemModel
+    {
+        [DisplayName("Product Id")]
+        public int ProductId { get; set; }
+        [DisplayName("Item Id")]
+        public int ItemId { get; set; }
+
+        [DisplayName("Product Title")]
+        public string ProductTitle { get; set; }
+        [DisplayName("Item Title")]
+        public string ItemTitle { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+        public ICollection<Item> Items { get; set; }
+       
+    }
+}
---
+++ Memberships/Extensions/ConversionExtensions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Memberships.Areas.Admin.Models;
+using System.Threading.Tasks;
+using System.Collections;
+using Memberships.Entities;
+using Memberships.Models;
+using System.Data.Entity;
+using Microsoft.Ajax.Utilities;
+
+namespace Memberships.Extensions
+{
+    public static class ConversionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> Convert(this IEnumerable<Product> products, ApplicationDbContext db)
+        {
+            if (products.Count().Equals(0))
+            {
+                return new List<ProductModel>();
+            }
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = texts,
+                       ProductTypes = types
+                   };
+        }
+
+        public static async Task<ProductModel> Convert(this Product product, ApplicationDbContext db)
+        {
+            var text = await db.ProductLinkTexts.FirstOrDefaultAsync(p => p.Id.Equals(product.ProductLinkTextId));
+            var type = await db.ProductTypes.FirstOrDefaultAsync(p => p.Id.Equals(product.ProductTypeId));
+            var model = new ProductModel
+            {
+                Id = product.Id,
+                Title = product.Title,
+                Description = product.Description,
+                ImageUrl = product.ImageUrl,
+                ProductLinkTextId = product.ProductLinkTextId,
+                ProductTypeId = product.ProductTypeId,
+                ProductLinkTexts = new List<ProductLinkText>(),
+                ProductTypes = new List<ProductType>()
+            };
+            model.ProductLinkTexts.Add(text);
+            model.ProductTypes.Add(type);
+            return model;
+        }
+
+        public static async Task<IEnumerable<ProductItemModel>> Convert(this IQueryable<ProductItem> productItems,
+            ApplicationDbContext db)
+        {
+            if (productItems.Count().Equals(0))
+            {
+                return new List<ProductItemModel>();
+            }
+
+            return await (from pi in productItems
+                select new ProductItemModel
+                {
+                    ProductId = pi.ProductId,
+                    ItemId = pi.ItemId,
+                    ItemTitle = db.Items.FirstOrDefault(i => i.Id.Equals(pi.ItemId)).Title,
+                    ProductTitle = db.Products.FirstOrDefault(p => p.Id.Equals(pi.ProductId)).Title
+                }).ToListAsync();
+        }
+
+        public static async Task<ProductItemModel> Convert(this ProductItem productItem, ApplicationDbContext db)
+        {
+            var model = new ProductItemModel
+            {
+                ItemId = productItem.ItemId,
+                ProductId = productItem.ProductId,
+                Items = await db.Items.ToListAsync(),
+                Products = await db.Products.ToListAsync()
+            };
+            return model;
+        }
+
+        public static async Task<bool> CanChange(this ProductItem productItem, ApplicationDbContext db)
+        {
+            var oldPI = await db.ProductItems.CountAsync(pi => pi.ProductId.Equals(productItem.OldProductId) && pi.ItemId.Equals(productItem.OldItemId));
+            var newPI = await db.ProductItems.CountAsync(pi => pi.ProductId.Equals(productItem.ProductId) && pi.ItemId.Equals(productItem.ItemId));
+            if (oldPI )
+            {
+                
+            }
+        };
+    }
+}
